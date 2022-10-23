9e71e9c13f5d85e39e9767663b34ea85d9dd35da
Pritam Samui <77156502+Pritamart@users.noreply.github.com>
2022-10-23 07:11:44 +0530

    Update day3.c

--- day3.c
+++ day3.c
@@ -1,18 +1,18 @@
-/*
+ /*
 author - Pritam Samui
 date - 23/11/2022
 WAP to swap two number using variables.
 */  
 #include <stdio.h>
 int main(){
-    int a,b,temp;
-    printf("Enther tow number\t");
-    scanf("%d%d",&a,&b);
-    printf("The valus of before swap a = %d \t b = %d\n",a,b);
+    int a, b, temp;
+    printf("Enther tow number \t");
+    scanf("%d%d", &a, &b);
+    printf("The valus of before swap a = %d \t b = %d\n", a, b);
     temp = a;
     a = b;
     b = temp;
-    printf("The valus of after swap a = %d \t b = %d\n",a,b);
+    printf("The valus of after swap a = %d \t b = %d\n", a, b);
     return 0;
 }
 /*Output
