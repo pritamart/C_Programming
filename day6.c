70805e948a0f55a3498667d8540e6d717d72c0b4
Pritam Samui <77156502+Pritamart@users.noreply.github.com>
2022-10-26 11:16:55 +0530

    Update day6.c

--- day6.c
+++ day6.c
@@ -5,8 +5,8 @@ WAP to student result as follow.
 Marks           gated
 80 >=           A
 60 >=           B
-50 >=           C
-40 <           D
+40 >=           C
+40 <            D
 */ 
 #include <stdio.h>
 int main()
@@ -22,7 +22,7 @@ int main()
         else if(n >= 60)
             printf("B");
         
-        else if(n >= 50)
+        else if(n >= 40)
             printf("C");
 
         else
@@ -33,18 +33,3 @@ int main()
         printf("Please enter marks (between 0 to 100) ");
   return 0;
  }
-/*output
-PS D:\education\c> cd "d:\education\c\" ; if ($?) { gcc day6.c -o day6 } ; if ($?) { .\day6 }
-Enter the marks out of 100 :-1023
-PS D:\education\c> cd "d:\education\c\" ; if ($?) { gcc day6.c -o day6 } ; if ($?) { .\day6 }
-Enter the marks out of 100 :-89
-PS D:\education\c> cd "d:\education\c\" ; if ($?) { gcc day6.c -o day6 } ; if ($?) { .\day6 }
-Enter the marks out of 100 :-70
-your grade is :- B
-PS D:\education\c> cd "d:\education\c\" ; if ($?) { gcc day6.c -o day6 } ; if ($?) { .\day6 }
-Enter the marks out of 100 :-56
-your grade is :- C
-PS D:\education\c> cd "d:\education\c\" ; if ($?) { gcc day6.c -o day6 } ; if ($?) { .\day6 }
-Enter the marks out of 100 :-45
-your grade is :- D
-*/
